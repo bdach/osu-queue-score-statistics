a48fed4221ac83224af900a9cc0259047709cc26
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2023-12-29 09:18:06 +0100

    Mark more mods invalid for performance if customised
    Closes https://github.com/ppy/osu-queue-score-statistics/issues/191.

--- osu.Server.Queues.ScoreStatisticsProcessor/Processors/ScorePerformanceProcessor.cs
+++ osu.Server.Queues.ScoreStatisticsProcessor/Processors/ScorePerformanceProcessor.cs
@@ -165,53 +165,38 @@ namespace osu.Server.Queues.ScoreStatisticsProcessor.Processors
                     case ManiaModKey10:
                         return false;
 
-                    case ModNightcore nc:
-                        // Disallow non-default rate adjustments for now.
-                        if (!nc.SpeedChange.IsDefault)
-                            return false;
-
-                        continue;
-
-                    case ModDoubleTime dt:
-                        // Disallow non-default rate adjustments for now.
-                        if (!dt.SpeedChange.IsDefault)
-                            return false;
-
-                        continue;
-
-                    case ModDaycore dc:
-                        // Disallow non-default rate adjustments for now.
-                        if (!dc.SpeedChange.IsDefault)
-                            return false;
-
-                        continue;
-
-                    case ModHalfTime ht:
-                        // Disallow non-default rate adjustments for now.
-                        if (!ht.SpeedChange.IsDefault)
-                            return false;
-
-                        continue;
-
                     case ModClassic:
                         // Classic mod is only allowed if it's attached to legacy scores.
                         return score.IsLegacyScore;
 
-                    // The mods which are allowed.
-                    case ModEasy:
-                    case ModNoFail:
+                    // The mods which are allowed:
+
+                    // Regardless of settings
+                    // (because settings do not affect difficulty):
                     case ModSuddenDeath:
                     case ModPerfect:
+                    case ModMuted:
+                        continue;
+
+                    // Only with default settings:
+                    case ModNightcore:
+                    case ModDoubleTime:
+                    case ModDaycore:
+                    case ModHalfTime:
+                    case ModEasy:
+                    case ModNoFail:
                     case ModHardRock:
                     case ModHidden:
                     case ModFlashlight:
-                    case ModMuted:
                     // osu!-specific:
                     case OsuModSpunOut:
                     case OsuModTouchDevice:
                     // mania-specific:
                     case ManiaKeyMod:
                     case ManiaModMirror:
+                        if (!m.UsesDefaultConfiguration)
+                            return false;
+
                         continue;
 
                     // Any other mods not in the above list aren't allowed.
